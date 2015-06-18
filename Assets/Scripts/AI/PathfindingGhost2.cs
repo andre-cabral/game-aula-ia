d6e4e28cdaf2572e31653e388e77800cd41a5b53
andre-cabral <cabral.dev@gmail.com>
2015-06-18 06:19:50 -0300

    Ghost2 AI complete (patrol an area and follow the player if its near)

--- Assets/Scripts/AI/PathfindingGhost2.cs
+++ Assets/Scripts/AI/PathfindingGhost2.cs
@@ -3,42 +3,86 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PathfindingGhost2 : MonoBehaviour {
-
+	
 	public Transform startObjectTransform;
 	public Transform endObjectTransform;
 	public float objectThickness = 0.95f;
 	public float speed = 0.1f;
 	public float maxDistanceFromTarget = 0.3f;
+	public Vector3[] patrolPoints;
+	public float distanceToStartFollowingPlayer = 10f;
+	public float distanceToStopFollowingPlayer = 20f;
+	public int patrolIndex = 0;
 	public bool escaping = false;
-
+	public bool huntingPlayer = false;
+	
 	List<Node> allNodes = new List<Node>();
-
+	
 	public List<Node> path = new List<Node>();
-
+	
 	void Awake(){
 		GameObject[] nodesGameObjects = GameObject.FindGameObjectsWithTag(Tags.node);
 		foreach(GameObject nodeGameObject in nodesGameObjects){
 			allNodes.Add (new Node(nodeGameObject.transform.localPosition, objectThickness));
 		}
 		Node endNode = new Node(endObjectTransform.position, objectThickness);
-
+		
 		AllNodes.allNodes = allNodes;
 		AllNodes.endNode = endNode;
-
+		
 		foreach(Node node in allNodes){
 			node.InitNeighbours(objectThickness);
 		}
-
+		
 		AllNodes.allNodes = allNodes;
 	}
-
+	
 	void Update(){
-		if(!escaping){
+		CheckDistanceFromPlayer();
+
+		if(!escaping && huntingPlayer){
 			FollowPlayer();
-		}else{
+		}else if(!escaping && !huntingPlayer){
+			Patrol ();
+		}
+		else{
 			EscapeToFurthestNode();
 		}
+
 		Move();
+	}
+
+	void CheckDistanceFromPlayer(){
+		float distance = Vector2.Distance(startObjectTransform.position, endObjectTransform.position);
+		if(distance > distanceToStopFollowingPlayer){
+			huntingPlayer = false;
+
+			//go to closest patrol point
+			/*
+			float closestPointDistance = Vector2.Distance(startObjectTransform.position, patrolPoints[0]);
+			for(int i=0; i < patrolPoints.Length; i++){
+				if(closestPointDistance > Vector2.Distance(startObjectTransform.position, patrolPoints[0])){
+					patrolIndex = i;
+				}
+			}
+			*/
+
+		}
+		if(distance < distanceToStartFollowingPlayer){
+			huntingPlayer = true;
+		}
+	}
+
+	void Patrol(){
+		if(maxDistanceFromTarget >= Vector2.Distance(transform.position, patrolPoints[patrolIndex])){
+			if(patrolIndex < patrolPoints.Length - 1){
+				patrolIndex++;
+			}else{
+				patrolIndex = 0;
+			}
+		}
+
+		FindPath(startObjectTransform.position, patrolPoints[patrolIndex]);
 	}
 	
 	void FollowPlayer(){
@@ -56,7 +100,7 @@ public class PathfindingGhost2 : MonoBehaviour {
 		}
 		FindPath(startObjectTransform.position, mostDistantNode.nodePosition);
 	}
-
+	
 	//#####FINDPATH START
 	//###################
 	void FindPath(Vector3 startPosition, Vector3 endPosition){
@@ -65,9 +109,9 @@ public class PathfindingGhost2 : MonoBehaviour {
 		Node endNode = new Node(endPosition, objectThickness);
 		startNode.InitNeighbours(objectThickness);
 		//endNode.InitNeighbours(objectThickness);
-
+		
 		AllNodes.endNode = endNode;
-
+		
 		//create a List with all the nodes and includes the end node.
 		/*
 		List<Node> allNodesWithEndNode = new List<Node>(allNodes);
@@ -75,12 +119,12 @@ public class PathfindingGhost2 : MonoBehaviour {
 		*/
 		List<Node> openSet = new List<Node>();
 		HashSet<Node> closedSet = new HashSet<Node>();
-
+		
 		openSet.Add(startNode);
-
+		
 		while(openSet.Count > 0){
 			Node currentNode = openSet[0];
-
+			
 			//verifies all the possible nodes and uses the one with the smallest total distance
 			//or if the total distance is equal, selects the one that is closest to the end
 			for(int i=0; i < openSet.Count; i++){
@@ -90,89 +134,18 @@ public class PathfindingGhost2 : MonoBehaviour {
 					currentNode = openSet[i];
 				}
 			}
-
+			
 			//remove the selected node from the open array and send it to the closed array
 			openSet.Remove(currentNode);
 			closedSet.Add(currentNode);
-
+			
 			//finish the search after reaching the endnode
 			if(currentNode == endNode){
 				RetracePath(startNode, endNode);
 				return;
 			}
-
+			
 			foreach(Node neighbour in currentNode.getNeighbours(endNode, escaping)){
-				//go to the next node if the node is in the closedset
-				if (closedSet.Contains(neighbour)) {
-					continue;
-				}
-
-				//checks if the neighbour startdistance is greater than the start distance by the current node (in case the node was
-				//reached by a longer way
-				//after that, calculates the enddistance and add the current node as the parent
-				//in the end, add the node to the open node (if the node wasnt in it already)
-				float newStartDistance = currentNode.startDistance + Vector2.Distance(currentNode.nodePosition, neighbour.nodePosition);
-				if(newStartDistance < neighbour.startDistance || !openSet.Contains(neighbour)){
-					neighbour.startDistance = newStartDistance;
-					neighbour.endDistance = Vector2.Distance(neighbour.nodePosition, endNode.nodePosition);
-					neighbour.parent = currentNode;
-
-					if (!openSet.Contains(neighbour))
-						openSet.Add(neighbour);
-				}
-
-			}
-
-		}
-	}
-	//#####FINDPATH END
-	//#################
-
-	//#####FINDPATHESCAPE START
-	//###################
-	void FindPathEscape(Vector3 startPosition, Vector3 endPosition){
-		//transforms the start and end objects positions into Node objects
-		Node startNode = new Node(startPosition, objectThickness);
-		Node endNode = new Node(endPosition, objectThickness);
-
-		List<Node> allNodesWithEndNode = allNodes;
-		allNodesWithEndNode.Add(endNode);
-
-		//create a List with all the nodes and includes the end node.
-		/*
-		List<Node> allNodesWithEndNode = new List<Node>(allNodes);
-		allNodesWithEndNode.Add(endNode);
-		*/
-		List<Node> openSet = new List<Node>();
-		HashSet<Node> closedSet = new HashSet<Node>();
-		
-		openSet.Add(startNode);
-		
-		while(openSet.Count > 0){
-			Node currentNode = openSet[0];
-			
-			//verifies all the possible nodes and uses the one with the smallest total distance
-			//or if the total distance is equal, selects the one that is closest to the end
-			for(int i=0; i < openSet.Count; i++){
-				if (openSet[i].totalDistance < currentNode.totalDistance || 
-				    openSet[i].totalDistance == currentNode.totalDistance && 
-				    openSet[i].endDistance < currentNode.endDistance){
-					currentNode = openSet[i];
-				}
-			}
-			
-			//remove the selected node from the open array and send it to the closed array
-			openSet.Remove(currentNode);
-			closedSet.Add(currentNode);
-			
-			//finish the search after reaching the endnode
-			if(currentNode == endNode){
-				RetracePath(startNode, endNode);
-				return;
-			}
-			
-			
-			foreach(Node neighbour in currentNode.getNeighbours(endNode, true)){
 				//go to the next node if the node is in the closedset
 				if (closedSet.Contains(neighbour)) {
 					continue;
@@ -196,15 +169,16 @@ public class PathfindingGhost2 : MonoBehaviour {
 			
 		}
 	}
-	//#####FINDPATHESCAPE END
+	//#####FINDPATH END
 	//#################
-
+	
+	
 	void RetracePath(Node startNode, Node endNode){
 		//list to put all the nodes in order
 		List<Node> path = new List<Node>();
-
+		
 		Node currentNode = endNode;
-
+		
 		//checks node by node, and put them on the path list, starting with the endnode
 		//it checks node by node getting their parents
 		while(currentNode != startNode){
@@ -212,10 +186,10 @@ public class PathfindingGhost2 : MonoBehaviour {
 			currentNode = currentNode.parent;
 		}
 		path.Reverse();
-
+		
 		this.path = path;
 	}
-
+	
 	void Move(){
 		if(path.Count > 0){
 			if(!(maxDistanceFromTarget >= Vector2.Distance(transform.position, path[0].nodePosition)) ){
@@ -224,5 +198,5 @@ public class PathfindingGhost2 : MonoBehaviour {
 			}
 		}
 	}
-
+	
 }
--- Assets/Scripts/AI/PathfindingGhost4.cs
+++ Assets/Scripts/AI/PathfindingGhost4.cs
@@ -3,35 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PathfindingGhost4 : MonoBehaviour {
-
+	
 	public Transform startObjectTransform;
 	public Transform endObjectTransform;
 	public float objectThickness = 0.95f;
 	public float speed = 0.1f;
 	public float maxDistanceFromTarget = 0.3f;
 	public bool escaping = false;
-
+	
 	List<Node> allNodes = new List<Node>();
-
+	
 	public List<Node> path = new List<Node>();
-
+	
 	void Awake(){
 		GameObject[] nodesGameObjects = GameObject.FindGameObjectsWithTag(Tags.node);
 		foreach(GameObject nodeGameObject in nodesGameObjects){
 			allNodes.Add (new Node(nodeGameObject.transform.localPosition, objectThickness));
 		}
 		Node endNode = new Node(endObjectTransform.position, objectThickness);
-
+		
 		AllNodes.allNodes = allNodes;
 		AllNodes.endNode = endNode;
-
+		
 		foreach(Node node in allNodes){
 			node.InitNeighbours(objectThickness);
 		}
-
+		
 		AllNodes.allNodes = allNodes;
 	}
-
+	
 	void Update(){
 		if(!escaping){
 			FollowPlayer();
@@ -56,7 +56,7 @@ public class PathfindingGhost4 : MonoBehaviour {
 		}
 		FindPath(startObjectTransform.position, mostDistantNode.nodePosition);
 	}
-
+	
 	//#####FINDPATH START
 	//###################
 	void FindPath(Vector3 startPosition, Vector3 endPosition){
@@ -65,9 +65,9 @@ public class PathfindingGhost4 : MonoBehaviour {
 		Node endNode = new Node(endPosition, objectThickness);
 		startNode.InitNeighbours(objectThickness);
 		//endNode.InitNeighbours(objectThickness);
-
+		
 		AllNodes.endNode = endNode;
-
+		
 		//create a List with all the nodes and includes the end node.
 		/*
 		List<Node> allNodesWithEndNode = new List<Node>(allNodes);
@@ -75,12 +75,12 @@ public class PathfindingGhost4 : MonoBehaviour {
 		*/
 		List<Node> openSet = new List<Node>();
 		HashSet<Node> closedSet = new HashSet<Node>();
-
+		
 		openSet.Add(startNode);
-
+		
 		while(openSet.Count > 0){
 			Node currentNode = openSet[0];
-
+			
 			//verifies all the possible nodes and uses the one with the smallest total distance
 			//or if the total distance is equal, selects the one that is closest to the end
 			for(int i=0; i < openSet.Count; i++){
@@ -90,89 +90,18 @@ public class PathfindingGhost4 : MonoBehaviour {
 					currentNode = openSet[i];
 				}
 			}
-
+			
 			//remove the selected node from the open array and send it to the closed array
 			openSet.Remove(currentNode);
 			closedSet.Add(currentNode);
-
+			
 			//finish the search after reaching the endnode
 			if(currentNode == endNode){
 				RetracePath(startNode, endNode);
 				return;
 			}
-
+			
 			foreach(Node neighbour in currentNode.getNeighbours(endNode, escaping)){
-				//go to the next node if the node is in the closedset
-				if (closedSet.Contains(neighbour)) {
-					continue;
-				}
-
-				//checks if the neighbour startdistance is greater than the start distance by the current node (in case the node was
-				//reached by a longer way
-				//after that, calculates the enddistance and add the current node as the parent
-				//in the end, add the node to the open node (if the node wasnt in it already)
-				float newStartDistance = currentNode.startDistance + Vector2.Distance(currentNode.nodePosition, neighbour.nodePosition);
-				if(newStartDistance < neighbour.startDistance || !openSet.Contains(neighbour)){
-					neighbour.startDistance = newStartDistance;
-					neighbour.endDistance = Vector2.Distance(neighbour.nodePosition, endNode.nodePosition);
-					neighbour.parent = currentNode;
-
-					if (!openSet.Contains(neighbour))
-						openSet.Add(neighbour);
-				}
-
-			}
-
-		}
-	}
-	//#####FINDPATH END
-	//#################
-
-	//#####FINDPATHESCAPE START
-	//###################
-	void FindPathEscape(Vector3 startPosition, Vector3 endPosition){
-		//transforms the start and end objects positions into Node objects
-		Node startNode = new Node(startPosition, objectThickness);
-		Node endNode = new Node(endPosition, objectThickness);
-
-		List<Node> allNodesWithEndNode = allNodes;
-		allNodesWithEndNode.Add(endNode);
-
-		//create a List with all the nodes and includes the end node.
-		/*
-		List<Node> allNodesWithEndNode = new List<Node>(allNodes);
-		allNodesWithEndNode.Add(endNode);
-		*/
-		List<Node> openSet = new List<Node>();
-		HashSet<Node> closedSet = new HashSet<Node>();
-		
-		openSet.Add(startNode);
-		
-		while(openSet.Count > 0){
-			Node currentNode = openSet[0];
-			
-			//verifies all the possible nodes and uses the one with the smallest total distance
-			//or if the total distance is equal, selects the one that is closest to the end
-			for(int i=0; i < openSet.Count; i++){
-				if (openSet[i].totalDistance < currentNode.totalDistance || 
-				    openSet[i].totalDistance == currentNode.totalDistance && 
-				    openSet[i].endDistance < currentNode.endDistance){
-					currentNode = openSet[i];
-				}
-			}
-			
-			//remove the selected node from the open array and send it to the closed array
-			openSet.Remove(currentNode);
-			closedSet.Add(currentNode);
-			
-			//finish the search after reaching the endnode
-			if(currentNode == endNode){
-				RetracePath(startNode, endNode);
-				return;
-			}
-			
-			
-			foreach(Node neighbour in currentNode.getNeighbours(endNode, true)){
 				//go to the next node if the node is in the closedset
 				if (closedSet.Contains(neighbour)) {
 					continue;
@@ -196,15 +125,16 @@ public class PathfindingGhost4 : MonoBehaviour {
 			
 		}
 	}
-	//#####FINDPATHESCAPE END
+	//#####FINDPATH END
 	//#################
-
+	
+	
 	void RetracePath(Node startNode, Node endNode){
 		//list to put all the nodes in order
 		List<Node> path = new List<Node>();
-
+		
 		Node currentNode = endNode;
-
+		
 		//checks node by node, and put them on the path list, starting with the endnode
 		//it checks node by node getting their parents
 		while(currentNode != startNode){
@@ -212,10 +142,10 @@ public class PathfindingGhost4 : MonoBehaviour {
 			currentNode = currentNode.parent;
 		}
 		path.Reverse();
-
+		
 		this.path = path;
 	}
-
+	
 	void Move(){
 		if(path.Count > 0){
 			if(!(maxDistanceFromTarget >= Vector2.Distance(transform.position, path[0].nodePosition)) ){
@@ -224,5 +154,5 @@ public class PathfindingGhost4 : MonoBehaviour {
 			}
 		}
 	}
-
+	
 }
